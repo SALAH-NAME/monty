7866a6d5a0c6749ddffcc20ee5ca648e19284137
SALAH-NAME <phone4ouahidi@gmail.com>
2023-05-28 22:08:37 +0000

    fix pop memory

--- helper_2.c
+++ helper_2.c
@@ -9,16 +9,17 @@
 */
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp;
 	if (!(*stack))
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->next)
-	{
-		(*stack)->next->prev = NULL;
-	}
-	(*stack) = (*stack)->next;
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack)
+		(*stack)->prev = NULL;
+	free(temp);
 }
 
 /**
@@ -59,7 +60,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack) || (!(*stack)->next))
 	{
-		fprintf(stderr, "L%u: can't add an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n += (*stack)->n;
